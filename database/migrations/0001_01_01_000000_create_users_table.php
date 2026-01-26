72a975457607ff5ea08e6a42e53c805d185a6918
Daniel Santos <daniel.oliveira@abril.com.br>
2026-01-26 18:06:04 -0300

    ajuste  iniciais nas models

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,10 +12,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('role_id')->constrained();
+            $table->foreignId('level_id')->constrained();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('phone')->unique()->nullable();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->integer('daily_target_minutes')->nullable();
+            $table->time('preferred_start_time')->nullable();
+            $table->json('preferred_days')->nullable();
+            $table->timestamp('onboarding_completed_at')->nullable();
+            $table->timestamp('last_message_at')->nullable();
+            $table->json("state")->default(json_encode(['state' => 'onboarding', 'status' => 'pending']));
             $table->rememberToken();
             $table->timestamps();
         });
